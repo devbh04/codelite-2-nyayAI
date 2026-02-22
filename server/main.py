d2e4accf7fa6c5fc8581c18bd02db64cb99b9270
devbh04 <devbhangale.work@gmail.com>
2026-02-22 06:32:03 +0530

    fix CORS for production

--- server/main.py
+++ server/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="NyayaAI API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
